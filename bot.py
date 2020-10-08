3e4473fc2f5e82f5ac72ff8dd65ab45ff99300f8
dianaavila2018 <45690382+dianaavila2018@users.noreply.github.com>
2020-10-07 21:26:12 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,15 +8,16 @@ STATE2 = 2
 
 def inicio(update, context):
     try:
-        usuario = update.message.from_user.usuario
-        message = 'Olá, ' + usuario + '!'
+        first_name = update.message.from_user.first_name
+        message = 'Olá, ' + username + '!'
         context.bot.send_message(chat_id=update.effective_chat.id, text=message)
     except Exception as error:
         print(str(error))
 
 def resposta(update, context):
     try:
-        message = '''Que informação deseja ?\n
+        message =
+        '''Que informação deseja ?\n
         1 - Cuidados com felinos\n
         2 - Alimentação\n
         3 - Castração\n
@@ -24,30 +25,30 @@ def resposta(update, context):
         # envia a mesagem para o usuario e guarda a resposta dele 
         update.message.reply_text(message, reply_markup=ReplyKeyboardMarkup([], one_time_keyboard=True)) 
         return STATE1
-    except Exception as error:
-        print(str(error))
+    except Exception as e:
+        print(str(e))
 
 
 def resposta1(update, context):
     resposta = update.message.text
     print(resposta)
     if (resposta=='1' or resposta=='cuidados' or resposta=='cuidar'):
-        message ='''
-        Ok.. vamos falar sobre cuidados com seus peludinhos\n'''
+        message =''' Ok.. vamos falar sobre cuidados com seus peludinhos\n'''
         context.bot.send_message(chat_id=update.effective_chat.id, text=message)
         return STATE2
     elif (resposta=='2' or resposta=='alimentacao' or resposta=='alimentação' or resposta=='alimento'
                 or resposta=='ração' or resposta=='racao' or resposta=='comida' ):
-        message = '''
-        Ok.. vamos falar sobre a alimentação dos seus peludinhos\n'''
+        message = '''  Ok.. vamos falar sobre a alimentação dos seus peludinhos\n'''
         context.bot.send_message(chat_id=update.effective_chat.id, text=message)
         return STATE2
     elif (resposta=='3' or resposta=='castração' or resposta=='castracao' or resposta=='castrar'
                 or resposta=='castrado'):
-        message = '''
-        Ok.. vamos falar sobre castração\n'''
+        message = '''  Ok.. vamos falar sobre castração\n'''
         context.bot.send_message(chat_id=update.effective_chat.id, text=message)
-        return STATE2    
+        return STATE2
+    else:
+        context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+        return ConversationHandler.END
 
 
 def resposta2(update, context):
@@ -70,7 +71,7 @@ def main():
         updater.dispatcher.add_handler(CommandHandler('start', inicio))
 
         conversation_handler = ConversationHandler(
-            entry_points=[CommandHandler('resposta', resposta)],
+            entry_points=[CommandHandler('feedback', resposta)],
             states={
                 STATE1: [MessageHandler(Filters.text, resposta1)],
                 STATE2: [MessageHandler(Filters.text, resposta2)]
@@ -81,8 +82,9 @@ def main():
         print("Updater no ar: " + str(updater))
         updater.start_polling()
         updater.idle()
-    except Exception as error:
-        print(str(error))
+    except Exception as e:
+        print(str(e))
 
 if __name__ == "__main__":
     main()
+
